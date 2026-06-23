5aac249f708352592eb967c792fde929eabcf440
bayuforge <bayuanataw@gmail.com>
2026-06-23 23:23:53 +0800

    Implement matrix operations: multiplication, Hadamard, addition

---
+++ Matrix/operation_on_matrix/operation.py
@@ -0,0 +1,60 @@
+def classic_multiplication(A:list, B:list):
+    C = [0, 0]
+    B_dash = [] # another matrix to store the outer layer of matrix B
+    first_matrix_length = len(A)
+    second_matrix_length = len(B)
+
+    # check outer layer for the first matrix
+    first_outer = A[0] # outer of the left-side is the first row
+    
+    # check outer layer for second matrix
+    for i in range(second_matrix_length):
+        B_dash.append(B[i][-1]) # perform operation to find the last node in the matrix for each column
+    second_outer = B_dash
+
+    if len(first_outer) == len(second_outer):
+        # make an empty matrix C with the right size to fill in the transformation
+        C = [[0 for _ in range(len(first_outer))] for _ in range(len(second_outer))]
+        
+        # do the transformation
+        for i in range(first_matrix_length):
+            for j in range(second_matrix_length):
+                C[i][j] = A[i][j] * B[j][i]
+        
+        return C
+    else:
+        print(first_outer, second_outer)
+        print("Math error. Both matrix are not in the same dimensions!")
+
+
+# Hadamard product is an element-wise multiplication operation on matrix
+def hadamard_product(A:list, B:list):
+    C:list = A
+    first_matrix_length = len(A)
+    second_matrix_length = len(B)
+
+    if first_matrix_length != second_matrix_length:
+        return "You need to have both the same matrix size! Hadamard Product is an element-wise operations."
+    else:
+        # element-wise multiplication operations
+        for i in range(first_matrix_length):
+            for j in range(second_matrix_length):
+                C[i][j] = A[i][j] * B[j][i]
+        
+        return C
+
+
+def addition(A:list, B:list):
+    C:list = A
+    first_matrix_length = len(A)
+    second_matrix_length = len(B)
+    
+    if first_matrix_length != second_matrix_length:
+        return "You need to have both the same matrix size! Matrix addition is an element-wise operations."
+    else:
+        # element-wise addition operations
+        for i in range(first_matrix_length):
+            for j in range(second_matrix_length):
+                C[i][j] = A[i][j] + B[j][i]
+        
+        return C
