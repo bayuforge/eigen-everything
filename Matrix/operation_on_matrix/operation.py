1a0af7f2275ddfc8430c2794ddb9c0b7bff16f01
bayuforge <bayuanataw@gmail.com>
2026-07-04 08:45:58 +0800

    Refactor matrix multiplication and add utility class
    Refactored classic_multiplication function to improve matrix multiplication logic and added matrixTools class for dimension calculation and column traversal.

--- Matrix/operation_on_matrix/operation.py
+++ Matrix/operation_on_matrix/operation.py
@@ -1,38 +1,21 @@
 def classic_multiplication(A:list, B:list):
-    c = [0, 0]
-    c_prime = [0, 0] # for storing the object
-    A_dash = [] # another matrix to store the outer layer of matrix B
-    first_matrix_length = len(A)
-    second_matrix_length = len(B)
+    a_dimension = matrixTools.dimension_calculator(A) # row x col -> dict
+    b_dimension = matrixTools.dimension_calculator(B) # row x col -> dict
+    c_prime = [[0 for x in range(a_dimension["row"])] for x in range(b_dimension["column"])]
+    c = []
 
-    # check outer layer for second matrix
-    for i in range(first_matrix_length):
-        A_dash.append(A[i][1]) # perform operation to find the last node in the matrix for each column
-    first_outer = A_dash
+    # row multiplication (dot product)
+    for i in range(a_dimension["row"]):
+        for j in range(b_dimension["column"]):
+            c_prime[i] = B[j][i] * A[i][j]
+            c_prime[i] = B[j+1][i] * A[i][j+1]
 
-    # check outer layer for the first matrix
-    second_outer = B[0] # outer of the left-side is the first row
-
-    length_a = len(first_outer)
-    length_b = len(second_outer)
-
-    if len(first_outer) == len(second_outer):
-        # make an empty matrix C with the right size to fill in the transformation
-        c_prime = [[0 for _ in range(len(first_outer))] for _ in range(len(second_outer))]
-        #print(f"Empty C matrix: {C}")
-        
-        # do the transformation
-        for i in range(length_a):
-            for j in range(length_b):
-                c_prime[i][j] = A[i][j] * B[i][j]
-            for k in range(length_b):
-                c[i][k] = c_prime[i][k] + c_prime[k][i]
-
-        return c_prime
-    else:
-        print(first_outer, second_outer)
-        print("Math error. Both matrix are not in the same dimensions!")
-
+    # sum of dot product
+    for m in range(a_dimension["row"]):
+        for n in range(b_dimension["column"]):
+            c[m][n] = c_prime[m][n] + c_prime[m][n+1]
+    
+    return c
 
 # Hadamard product is an element-wise multiplication operation on matrix
 def hadamard_product(A:list, B:list):
@@ -65,3 +48,21 @@ def addition(A:list, B:list):
                 C[i][j] = A[i][j] + B[i][j]
         
         return C
+
+class matrixTools():
+    def column_traversal(a_dash:list):
+        result = [row[0] for row in a_dash]
+
+        return result
+
+    def dimension_calculator(A:list):
+        a_dash = [s[0] for s in A]
+        
+        dimension = {
+            "row": len(a_dash),
+            "column": int(len(A[0]))
+        }
+
+        return dimension
+    
+    #def dot_product_summation(A:list):
